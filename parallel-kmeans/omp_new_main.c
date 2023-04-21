fcd38c3b34968565412de8874e7966561ecfa1bd
bdavis12302 <blakedavis12302@gmail.com>
2023-04-21 12:18:58 -0600

    Big Changes

--- parallel-kmeans/omp_new_main.c
+++ parallel-kmeans/omp_new_main.c
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-/*   File:         seq_main.c   (an sequential version)                      */
+/*   File:         omp_main.c   (an OpenMP version)                          */
 /*   Description:  This program shows an example on how to call a subroutine */
 /*                 that implements a simple k-means clustering algorithm     */
 /*                 based on Euclid distance.                                 */
@@ -26,8 +26,17 @@
 #include <fcntl.h>
 #include <unistd.h>     /* getopt() */
 
+#include <omp.h>
 int      _debug;
 #include "kmeans.h"
+
+#ifdef _PNETCDF_BUILT
+#include <mpi.h>
+#include <pnetcdf.h>
+float** pnetcdf_read(char*, char*, int*, int*, MPI_Comm);
+int     pnetcdf_write(char*, int, int, int, int, float**, int*, int, MPI_Comm,
+                      int verbose);
+#endif
 
 /*---< usage() >------------------------------------------------------------*/
 static void usage(char *argv0, float threshold) {
@@ -38,7 +47,11 @@ static void usage(char *argv0, float threshold) {
         "       -b             : input file is in binary format (default no)\n"
         "       -n num_clusters: number of clusters (K must > 1)\n"
         "       -t threshold   : threshold value (default %.4f)\n"
+        "       -p nproc       : number of threads (default system allocated)\n"
+        "       -a             : perform atomic OpenMP pragma (default no)\n"
         "       -o             : output timing results (default no)\n"
+        "       -c var_name    : using PnetCDF for file input and output and var_name\n"
+        "                      : is variable name in the netCDF file to be clustered\n"
         "       -q             : quiet mode\n"
         "       -d             : enable debug mode\n"
         "       -h             : print this help information\n";
@@ -46,32 +59,44 @@ static void usage(char *argv0, float threshold) {
     exit(-1);
 }
 
-/*---< main() >-------------------------------------------------------------*/
+/*---< main() >---------------------------------------------------------------*/
 int main(int argc, char **argv) {
            int     opt;
     extern char   *optarg;
     extern int     optind;
-           int     i, j, isBinaryFile, is_output_timing, verbose;
+           int     i, j, nthreads, verbose;
+           int     isBinaryFile, is_perform_atomic, is_output_timing;
+           int     do_pnetcdf;
 
            int     numClusters, numCoords, numObjs;
            int    *membership;    /* [numObjs] */
            char   *filename, *center_filename;
+           char   *var_name;
            float **objects;       /* [numObjs][numCoords] data objects */
            float **clusters;      /* [numClusters][numCoords] cluster center */
            float   threshold;
            double  timing, io_timing, clustering_timing;
 
-    /* some default values */
-    _debug           = 0;
-    verbose          = 1;
-    threshold        = 0.001;
-    numClusters      = 0;
-    isBinaryFile     = 0;
-    is_output_timing = 0;
-    filename         = NULL;
-    center_filename  = NULL;
+#ifdef _PNETCDF_BUILT
+    MPI_Init(&argc, &argv);
+#endif
 
-    while ( (opt=getopt(argc,argv,"p:i:c:n:t:abdohq"))!= EOF) {
+    /* some default values */
+    _debug            = 0;
+    verbose           = 1;
+    nthreads          = 0;
+    numClusters       = 0;
+    threshold         = 0.001;
+    numClusters       = 0;
+    isBinaryFile      = 0;
+    is_output_timing  = 0;
+    is_perform_atomic = 0;
+    filename          = NULL;
+    do_pnetcdf        = 0;
+    var_name          = NULL;
+    center_filename   = NULL;
+
+    while ( (opt=getopt(argc,argv,"p:i:n:t:c:v:abdohq"))!= EOF) {
         switch (opt) {
             case 'i': filename=optarg;
                       break;
@@ -83,11 +108,18 @@ int main(int argc, char **argv) {
                       break;
             case 'n': numClusters = atoi(optarg);
                       break;
+            case 'p': nthreads = atoi(optarg);
+                      break;
+            case 'a': is_perform_atomic = 1;
+                      break;
             case 'o': is_output_timing = 1;
                       break;
             case 'q': verbose = 0;
                       break;
             case 'd': _debug = 1;
+                      break;
+            case 'v': do_pnetcdf = 1;
+                      var_name = optarg;
                       break;
             case 'h':
             default: usage(argv[0], threshold);
@@ -99,11 +131,29 @@ int main(int argc, char **argv) {
 
     if (filename == 0 || numClusters <= 1) usage(argv[0], threshold);
 
-    if (is_output_timing) io_timing = wtime();
+#ifndef _PNETCDF_BUILT
+    if (do_pnetcdf) {
+        printf("Error: PnetCDF feature is not built\n");
+        exit(1);
+    }
+#endif
+
+    /* set the no. threads if specified in command line, else use all
+       threads allocated by run-time system */
+    if (nthreads > 0)
+        omp_set_num_threads(nthreads);
+
+    if (is_output_timing) io_timing = omp_get_wtime();
 
     /* read data points from file ------------------------------------------*/
     printf("reading data points from file %s\n",filename);
 
+#ifdef _PNETCDF_BUILT
+    if (do_pnetcdf)
+        objects = pnetcdf_read(filename, var_name, &numObjs, &numCoords,
+                               MPI_COMM_WORLD);
+    else
+#endif
     objects = file_read(isBinaryFile, filename, &numObjs, &numCoords);
     if (objects == NULL) exit(1);
 
@@ -158,30 +208,36 @@ int main(int argc, char **argv) {
     }
 
     if (is_output_timing) {
-        timing            = wtime();
+        timing            = omp_get_wtime();
         io_timing         = timing - io_timing;
         clustering_timing = timing;
-    }
+    }      
 
-    /* start the timer for the core computation -----------------------------*/
+    /* start the core computation -------------------------------------------*/
     /* membership: the cluster id for each data object */
     membership = (int*) malloc(numObjs * sizeof(int));
     assert(membership != NULL);
 
-    seq_kmeans(objects, numCoords, numObjs, numClusters, threshold, membership,
-               clusters);
+    omp_kmeans(is_perform_atomic, objects, numCoords, numObjs,
+               numClusters, threshold, membership, clusters);
 
     free(objects[0]);
     free(objects);
 
     if (is_output_timing) {
-        timing            = wtime();
+        timing            = omp_get_wtime();
         clustering_timing = timing - clustering_timing;
-    }
+    }       
 
     /* output: the coordinates of the cluster centres ----------------------*/
-    file_write(filename, numClusters, numObjs, numCoords, clusters,
-               membership, verbose);
+#ifdef _PNETCDF_BUILT
+    if (do_pnetcdf)
+        pnetcdf_write(filename, 1, numClusters, numObjs, numCoords, clusters,
+                      membership, numObjs, MPI_COMM_SELF, verbose);
+    else
+#endif
+    file_write(filename, numClusters, numObjs, numCoords, clusters, membership,
+               verbose);
 
     free(membership);
     free(clusters[0]);
@@ -189,9 +245,15 @@ int main(int argc, char **argv) {
 
     /*---- output performance numbers ---------------------------------------*/
     if (is_output_timing) {
-        io_timing += wtime() - timing;
-        printf("\nPerforming **** Regular Kmeans (sequential version) ****\n");
+        io_timing += omp_get_wtime() - timing;
 
+        printf("\nPerforming **** Regular Kmeans  (OpenMP) ----");
+        if (is_perform_atomic)
+            printf(" using atomic pragma ******\n");
+        else
+            printf(" using array reduction ******\n");
+
+        printf("Number of threads = %d\n", omp_get_max_threads());
         printf("Input file:     %s\n", filename);
         printf("numObjs       = %d\n", numObjs);
         printf("numCoords     = %d\n", numCoords);
@@ -202,6 +264,9 @@ int main(int argc, char **argv) {
         printf("Computation timing = %10.4f sec\n", clustering_timing);
     }
 
+#ifdef _PNETCDF_BUILT
+    MPI_Finalize();
+#endif
     return(0);
 }
 
